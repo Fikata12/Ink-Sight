14477cc78f6942d7b9f74feb2afa83f53582fb66
Filip Karabashev <filipkarabashev@gmail.com>
2023-11-22 08:27:49 +0200

    Create not found component

---
+++ client/src/components/not-found/NotFound.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+import "/index.css";
+
+export default function NotFound() {
+    return (
+        <div className="text-center">
+            <h1 className="display-1 fw-bold">404</h1>
+            <p className="fs-3"> <span className="text-orange">Opps!</span> Page not found.</p>
+            <p className="lead">
+                The page you're looking for doesn't exist.
+            </p>
+            <Link className="btn color-orange" to="/">Go Home</Link>
+        </div>
+    )
+}
