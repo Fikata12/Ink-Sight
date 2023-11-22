bb89b04676c21cd6b31d1342a37e277ac109a1d6
Filip Karabashev <filipkarabashev@gmail.com>
2023-11-22 08:26:09 +0200

    Add routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,23 @@
+import {Route, Routes} from 'react-router-dom';
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
+import NotFound from './components/not-found/NotFound';
 
 function App() {
   return (
     <>
       <Header />
-      <Footer /> 
+      <Routes>
+        <Route path='/' />
+        <Route path='/reviews' />
+        <Route path='/add' />
+        <Route path='/about' />
+        <Route path='/edit/:id' />
+        <Route path='/login' />
+        <Route path='/register' />
+        <Route path='*' element={ <NotFound />} />
+      </Routes>
+      <Footer />
     </>
   )
 }
