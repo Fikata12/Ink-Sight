7fee4cd9e672b60d788a93f845f2b972c8740221
Filip Karabashev <filipkarabashev@gmail.com>
2023-11-22 23:44:53 +0200

    Style NotFound component

--- client/src/components/not-found/NotFound.jsx
+++ client/src/components/not-found/NotFound.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+
+import "./NotFound.css";
 import "/index.css";
 
 export default function NotFound() {
     return (
-        <div className="text-center">
+        <div className="text-center not-found">
             <h1 className="display-1 fw-bold">404</h1>
             <p className="fs-3"> <span className="text-orange">Opps!</span> Page not found.</p>
             <p className="lead">
