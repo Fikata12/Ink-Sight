3fcbf40200fece96b1eadc984e8fbf1e23af6345
Filip Karabashev <filipkarabashev@gmail.com>
2023-11-23 09:24:38 +0200

    Add routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 
+import "/index.css";
+
 import NotFound from './components/not-found/NotFound';
 import About from './components/about/About';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
-
-import "/index.css";
+import Login from './components/login/Login';
+import Register from './components/register/Register';
 
 function App() {
   return (
@@ -18,8 +20,8 @@ function App() {
           <Route path='/add' />
           <Route path='/about' element={<About />} />
           <Route path='/edit/:id' />
-          <Route path='/login' />
-          <Route path='/register' />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
           <Route path='*' element={<NotFound />} />
         </Routes>
       </div>
