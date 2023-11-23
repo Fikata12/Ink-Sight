42deceefcc1fa5704b10c38c22c281cc7f4404a4
Filip Karabashev <filipkarabashev@gmail.com>
2023-11-23 08:10:28 +0200

    Edit footer component

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -3,9 +3,9 @@ import "/index.css";
 
 export default function Footer() {
     return (
-        <footer class="border-top footer text-muted color-orange">
-            <div class="container">
-                <span class="footer-text">&copy; 2023 - InkSight</span>
+        <footer className="border-top footer text-muted color-orange">
+            <div className="container">
+                <span className="footer-text">&copy; 2023 - InkSight</span>
             </div>
         </footer>
     )
