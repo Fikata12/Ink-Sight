b573419d8f1aecfd6fe8b14787af3650858b8224
Filip Karabashev <filipkarabashev@gmail.com>
2023-11-23 08:10:18 +0200

    Edit about component

--- client/src/components/about/About.jsx
+++ client/src/components/about/About.jsx
@@ -6,11 +6,11 @@ export default function About() {
             <div className="container image-container my-4">
                 <img src="./public/big-logo.png" alt="Logo" />
             </div>
-            <section class="mt-5 py-4 color-light-orange">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-8 mx-auto">
-                            <h2 class="text-center mb-4">Who We Are</h2>
+            <section className="mt-5 py-4 color-light-orange">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-8 mx-auto">
+                            <h2 className="text-center mb-4">Who We Are</h2>
                             <p>
                                 Welcome to Ink Sight, where literature comes to life through the eyes of our diverse community of readers.
                                 Our platform is designed to connect book enthusiasts, share thoughts, and foster a love for reading.
@@ -23,11 +23,11 @@ export default function About() {
                     </div>
                 </div>
             </section>
-            <section class="py-4">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-8 mx-auto">
-                            <h2 class="text-center mb-4">Our Mission</h2>
+            <section className="py-4">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-8 mx-auto">
+                            <h2 className="text-center mb-4">Our Mission</h2>
                             <p>
                                 Our mission is to create a vibrant and inclusive community where people can share their passion for books.
                                 We believe in the power of storytelling to inspire, educate, and connect individuals from all walks of life.
