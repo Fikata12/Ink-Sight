51facf517606f8e2b009b0ecf5cb701d8d0ac7cd
Filip Karabashev <filipkarabashev@gmail.com>
2023-11-23 09:24:11 +0200

    Create Login component

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -5,7 +5,7 @@ import "/index.css"
 
 export default function Login() {
     return (
-        <form class="login-form mx-auto color-light-orange p-4 rounded border">
+        <form className="login-form mx-auto color-light-orange p-4 rounded border">
 
             <div className="container image-container my-2">
                 <img src="./public/small-logo.png" alt="Logo" />
@@ -13,19 +13,19 @@ export default function Login() {
 
             <h1 className="text-center mb-2">Login</h1>
 
-            <div class="form-outline mb-2">
-                <label class="form-label" for="EmailAddress">Email Address</label>
-                <input type="email" id="EmailAddress" class="form-control" />
+            <div className="form-outline mb-2">
+                <label className="form-label" htmlFor="EmailAddress">Email Address</label>
+                <input type="email" id="EmailAddress" className="form-control" />
             </div>
 
-            <div class="form-outline mb-3">
-                <label class="form-label" for="Password">Password</label>
-                <input type="password" id="Password" class="form-control" />
+            <div className="form-outline mb-3">
+                <label className="form-label" htmlFor="Password">Password</label>
+                <input type="password" id="Password" className="form-control" />
             </div>
 
-            <button type="button" class="btn color-dark-orange mb-2 d-block mx-auto w-50">Sign in</button>
+            <button type="button" className="btn color-dark-orange mb-2 d-block mx-auto w-50">Sign in</button>
 
-            <div class="text-center">
+            <div className="text-center">
                 <p>Not a member? <Link to={"/register"} className="text-orange text-dark-orange">Register</Link></p>
             </div>
         </form>
